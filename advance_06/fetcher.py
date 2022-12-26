5205123cf2c13718f118176418e0ae70e3582f3a
Matthew <55106609+IskornevME@users.noreply.github.com>
2022-12-26 11:39:38 +0300

    improve mistakes

--- advance_06/fetcher.py
+++ advance_06/fetcher.py
@@ -2,7 +2,6 @@ import time
 import sys
 import asyncio
 import aiohttp
-from aioresponses import aioresponses
 
 
 SYMBOL = "0"
@@ -17,19 +16,26 @@ async def fetch_url(url, session, res_dict):
         return num_zeros
 
 
-async def worker(queue, session, res_dict):
+async def worker(queue, session, res_dict, file, file_error=None):
     while True:
         url = await queue.get()
         try:
             await fetch_url(url, session, res_dict)
+        except:
+            print("Incorrect url")
+            if file_error:
+                print("Incorrect url", file=file_error)
         finally:
+            tmp = file.readline()
+            if tmp.rstrip() != "":
+                await queue.put(tmp.rstrip())
             queue.task_done()
 
 
-async def fetch_batch_urls(queue, num_workers, res_dict):
+async def fetch_batch_urls(queue, num_workers, res_dict, file, file_error=None):
     async with aiohttp.ClientSession() as session:
         tasks = [
-            asyncio.create_task(worker(queue, session, res_dict))
+            asyncio.create_task(worker(queue, session, res_dict, file, file_error))
             for _ in range(num_workers)
         ]
         await queue.join()
@@ -38,33 +44,18 @@ async def fetch_batch_urls(queue, num_workers, res_dict):
             task.cancel()
 
 
-async def main(file_name, num_workers, res_dict, mock_artefacts=None):
-    if mock_artefacts:
+async def main(file_name, num_workers, res_dict, file_error=None):
+    with open(file_name) as file:
         urls_queue = asyncio.Queue()
-        for elem in mock_artefacts:
-            await urls_queue.put(elem[0])
+        for _ in range(num_workers):
+            tmp = file.readline()
+            await urls_queue.put(tmp.rstrip())
 
-        with aioresponses() as mocked:
-            for elem in mock_artefacts:
-                mocked.get(elem[0], status=200, body=elem[1], repeat=True)
+        t_1 = time.time()
+        await fetch_batch_urls(urls_queue, num_workers, res_dict, file, file_error)
+        print("Time of fetching urls", time.time() - t_1)
+        print(f"Number of workers {num_workers}")
 
-            t_1 = time.time()
-            await fetch_batch_urls(urls_queue, num_workers, res_dict)
-            print("Time of fetching mocked urls", time.time() - t_1)
-            print(f"Number of workers {num_workers}")
-
-    else:
-        with open(file_name) as file:
-            lines = file.readlines()
-            n_urls = int(lines[0])
-            urls_queue = asyncio.Queue()
-            for i in range(1, n_urls + 1):
-                await urls_queue.put(lines[i].rstrip())
-
-            t_1 = time.time()
-            await fetch_batch_urls(urls_queue, num_workers, res_dict)
-            print("Time of fetching urls", time.time() - t_1)
-            print(f"Number of workers {num_workers}")
 
 if __name__ == '__main__':
     workers = int(sys.argv[1])
--- advance_06/test_fetcher.py
+++ advance_06/test_fetcher.py
@@ -3,44 +3,135 @@ This file tests program fetch urls
 '''
 import unittest
 import asyncio
+from io import StringIO
+import aiohttp
+from aioresponses import aioresponses
 
 import fetcher
 
 
 SYMBOL = "0"
-FILENAME = "urls.txt"
 
 
-class TestFetcher(unittest.TestCase):
+class TestFetcher(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.mock_artefacts = []
         with open("mock_artefacts.txt") as file:
             lines = file.readlines()
             for line in lines:
                 self.mock_artefacts.append(list(line.split(": ")))
+        with open("test_urls.txt", "w+") as test_file:
+            for elem in self.mock_artefacts:
+                test_file.writelines(str(elem[0]) + "\n")
+        self.filename = "test_urls.txt"
 
-    def test_urls_1_worker(self):
-        workers = 10
-        urls_dict = dict()
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(fetcher.main(FILENAME, workers, urls_dict, self.mock_artefacts))
-        get_res = []
-        true_res = []
-        for elem in self.mock_artefacts:
-            get_res.append(urls_dict[elem[0]])
-            true_res.append(elem[1].count(SYMBOL))
-        self.assertListEqual(true_res, get_res)
+    async def test_fetcher_1_work(self):
+        with aioresponses() as mocked:
+            for elem in self.mock_artefacts:
+                mocked.get(elem[0], status=200, body=elem[1], repeat=True)
+            workers = 1
+            urls_dict = dict()
+            await fetcher.main(self.filename, workers, urls_dict)
+            get_res = []
+            true_res = []
+            for elem in self.mock_artefacts:
+                get_res.append(urls_dict[elem[0]])
+                true_res.append(elem[1].count(SYMBOL))
+            self.assertListEqual(true_res, get_res)
 
-    def test_urls_20_worker(self):
-        print()
-        workers = 20
-        urls_dict = dict()
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(fetcher.main(FILENAME, workers, urls_dict, self.mock_artefacts))
-        loop.close()
-        get_res = []
-        true_res = []
-        for elem in self.mock_artefacts:
-            get_res.append(urls_dict[elem[0]])
-            true_res.append(elem[1].count(SYMBOL))
-        self.assertListEqual(true_res, get_res)
+    async def test_worker(self):
+        with open("test_urls.txt", "r+") as test_file:
+            with aioresponses() as mocked:
+                urls_queue = asyncio.Queue()
+                for elem in self.mock_artefacts:
+                    mocked.get(elem[0], status=200, body=elem[1], repeat=True)
+                res_dict = dict()
+                num_workers = 2
+                for _ in range(num_workers):
+                    tmp = test_file.readline()
+                    await urls_queue.put(tmp.rstrip())
+                async with aiohttp.ClientSession() as session:
+                    tasks = [
+                        asyncio.create_task(
+                            fetcher.worker(urls_queue, session, res_dict, test_file)
+                        )
+                        for _ in range(num_workers)
+                    ]
+                    await urls_queue.join()
+
+                    for task in tasks:
+                        task.cancel()
+                get_res = []
+                true_res = []
+                for elem in self.mock_artefacts:
+                    get_res.append(res_dict[elem[0]])
+                    true_res.append(elem[1].count(SYMBOL))
+                self.assertListEqual(true_res, get_res)
+
+    async def test_worker_exception(self):
+        excep_url = "https://incorrect_url_324edfewrr4"
+        with open("test_urls.txt", mode="a") as test_file:
+            test_file.writelines(excep_url + "\n")
+        self.mock_artefacts.append([excep_url, "df0sc"])
+        with open("test_urls.txt", "r+") as test_file:
+            with aioresponses() as mocked:
+                urls_queue = asyncio.Queue()
+                for elem in self.mock_artefacts:
+                    if elem[0] != excep_url:
+                        mocked.get(elem[0], status=200, body=elem[1], repeat=True)
+                res_dict = dict()
+                file_for_error = StringIO()
+
+                num_workers = 1
+                for _ in range(num_workers):
+                    tmp = test_file.readline()
+                    await urls_queue.put(tmp.rstrip())
+                async with aiohttp.ClientSession() as session:
+                    tasks = [
+                        asyncio.create_task(
+                            fetcher.worker(urls_queue, session, res_dict, test_file, file_for_error)
+                        )
+                        for _ in range(num_workers)
+                    ]
+                    await urls_queue.join()
+                    for task in tasks:
+                        task.cancel()
+                self.assertRaises(KeyError, lambda: res_dict[excep_url])
+                self.assertEqual(file_for_error.getvalue().rstrip(), "Incorrect url")
+
+    async def test_fetch_url(self):
+        with aioresponses() as mocked:
+            urls = [
+                ["http://example_1", "fe000rw0"],
+                ["http://example_2", " "],
+                ["http://example_3", "0 0000 w0"]
+                    ]
+            for elem in urls:
+                mocked.get(elem[0], status=200, body=elem[1], repeat=True)
+            res_dict = dict()
+            res_arr = []
+            async with aiohttp.ClientSession() as session:
+                for elem in urls:
+                    tmp = await fetcher.fetch_url(elem[0], session, res_dict)
+                    res_arr.append(tmp)
+            expected_dict = {
+                'http://example_1': 4,
+                'http://example_2': 0,
+                'http://example_3': 6
+            }
+            self.assertListEqual(res_arr, [4, 0, 6])
+            self.assertDictEqual(res_dict, expected_dict)
+
+    async def test_fetcher_20_work(self):
+        with aioresponses() as mocked:
+            for elem in self.mock_artefacts:
+                mocked.get(elem[0], status=200, body=elem[1], repeat=True)
+            workers = 20
+            urls_dict = dict()
+            await fetcher.main(self.filename, workers, urls_dict)
+            get_res = []
+            true_res = []
+            for elem in self.mock_artefacts:
+                get_res.append(urls_dict[elem[0]])
+                true_res.append(elem[1].count(SYMBOL))
+            self.assertListEqual(true_res, get_res)
